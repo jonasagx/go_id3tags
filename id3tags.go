84f17a4be42e1d4d91d704e1cdd87ee0b1a9dc3f
Rohan Deshmukh <pampasdev@gmail.com>
2016-03-14 22:06:04 +0530

    setID3Tags function now working

--- id3tags.go
+++ id3tags.go
@@ -1,20 +1,19 @@
-package main
+package id3tags
 
 import (
 	"fmt"
-	//"io/ioutil"
 	"os"
-	//"strings"
 	"log"
 )
 
-type mp3 struct {
-	filename string
-	path string
-	title  string
-	artist string
-	album  string
-	year   string
+//Mp3 ...Contains metadata of the file like title, artist, album, year info along with file name and its path
+type Mp3 struct {
+	Filename string
+	Path     string
+	Title    string
+	Artist   string
+	Album    string
+	Year     string
 }
 
 func getLastBytes(filename string) ([]byte, error) {
@@ -28,6 +27,9 @@ func getLastBytes(filename string) ([]byte, error) {
 	}
 	b := make([]byte, 128)
 	_, err = file.Read(b)
+    if err!=nil{
+        fmt.Println(err)
+    }
 	return b, nil
 }
 
@@ -47,17 +49,24 @@ func setLastBytes(filename string, b []byte)(error){
 	return nil
 }
 
-func (m *mp3) getID3Tags() {
-	b, _ := getLastBytes(m.path + m.filename)
+//GetID3Tags ...Extracts mp3 metadata from the file
+func (m *Mp3) GetID3Tags() {
+	b, _ := getLastBytes(m.Path + m.Filename)
 	if string(b[:3]) == "TAG" {
-		m.title = string(b[3:33])
-		m.artist = string(b[33:63])
-		m.album = string(b[63:93])
-		m.year = string(b[93:97])
-		fmt.Println(m.title,m.artist,m.album,m.year)
+		m.Title = string(b[3:33])
+		m.Artist = string(b[33:63])
+		m.Album = string(b[63:93])
+		m.Year = string(b[93:97])
+		fmt.Println(m.Title,m.Artist,m.Album,m.Year)
 	}
 }
-func (m *mp3) setID3Tags() {
-	b:=make([]byte,128)
-	setLastBytes(m.path+m.filename,b)
+//SetID3Tags ...Writes tag metadata to file
+func (m *Mp3) SetID3Tags() {
+	b := make([]byte,128)
+    copy(b[:],"TAG")
+    copy(b[3:33],m.Title)
+    copy(b[33:63],m.Artist)
+    copy(b[63:93],m.Album)
+    copy(b[93:97],m.Year)
+    setLastBytes(m.Path +m.Filename,b)
 }
